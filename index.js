38986d23f7c348d6fd2ab92d8768f1884874d10b
Radomir Pekotv <Radomirpetkov@abv.bg>
2022-05-29 17:39:32 +0300

    take info with post method and req.params

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const port = 3000
 const app = express()
+let people = []
 
 //Action
 app.get('/', (req, res) => {
@@ -11,7 +12,15 @@ app.get('/', (req, res) => {
     res.send(`Hello world`) //Combination of res.write and res.end
 })
 
-app.get('*', (req, res) =>{
+app.post(`/people/:person`, (req, res) => {
+    people.push(req.params.person)
+    res.send(`The following page has been visited: http://localhost:3000/people/${req.params.person}`)
+})
+
+app.get(`/people`, (req, res) => {
+    res.send(`A post method was sent to the following subpages: ${people.join(`, `)}`)
+})
+app.get('*', (req, res) => {
     res.status(404) // sends status 404 as a response. Otherwise its 200
     res.send(`Wrong page`) // Output when the path is wrong
 })
